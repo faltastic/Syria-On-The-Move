56926c2ecc687ed3000523a85b050907187dbb5a
vkollin <vkollin@gmail.com>
2015-12-08 14:57:25 +0100

    Style the navigation

--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -1,3 +1,11 @@
-<nav>
-    asdasd
+<nav id="navigation">
+    <ul>
+        <li class="logo"><a href="/">{!! HTML::image(asset('img/syriaonthemove.png')) !!}</a></li>
+        <li><a href="#">Map</a></li>
+        <li><a href="#">Why</a></li>
+        <li><a href="#">Workshops</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Link</a></li>
+    </ul>
 </nav>
