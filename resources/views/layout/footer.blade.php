3e659c42f079b5405ebfd58086caa3cad7779ce5
faltastic <youssef.faltas@gmail.com>
2015-12-17 15:46:17 +0100

    footer style

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,37 @@
 <footer>
-    <div class="c">
-        Footer
-    </div>
+
+      <div id="footer-container">
+       
+        
+
+        <div class="footer-column">
+
+        	<small> Lorem ipsum dolor sit amet, consectetuer adipisenim. Donec pede justo, fringilla vel, aliquet nec, vulputate eleifend tellus. Aenean leo ligula, portti-
+cing elit. Aenean commodo ligula eget dolor. Aene- vulputate eget, arcu. In enim justo, rhoncus ut tor eu, consequat vitae, eleifend ac, enim. Aliquam
+an massa. Cum sociis natoque penatibus et. </small>
+
+        </div>
+
+        <div class="footer-column">
+
+        	<small> Lorem ipsum dolor sit amet, consectetuer adipisenim. Donec pede justo, fringilla vel, aliquet nec, vulputate eleifend tellus. Aenean leo ligula, portti-
+cing elit. Aenean commodo ligula eget dolor. Aene- vulputate eget, arcu. In enim justo, rhoncus ut tor eu, consequat vitae, eleifend ac, enim. Aliquam
+an massa. Cum sociis natoque penatibus et. </small>
+
+        </div>
+        
+        <div class="footer-column">
+
+        	<small> Lorem ipsum dolor sit amet, consectetuer adipisenim. Donec pede justo, fringilla vel, aliquet nec, vulputate eleifend tellus. Aenean leo ligula, portti-
+cing elit. Aenean commodo ligula eget dolor. Aene- vulputate eget, arcu. In enim justo, rhoncus ut tor eu, consequat vitae, eleifend ac, enim. Aliquam
+an massa. Cum sociis natoque penatibus et. </small>
+
+        </div>
+
+   
+     
+   
+    
+    <div>
+
 </footer>
